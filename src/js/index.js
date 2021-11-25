97ca10d57f1a86637cac61c8ccbd47e52657ce83
albacandelario <alba.candelario@gracia.lasalle.cat>
2021-11-25 19:46:25 +0100

    pruebas fallidas

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,16 @@
 const form = document.querySelector('form');
-const button = document.querySelector('button');
+const button = document.getElementById('btnregister');
 const p = document.querySelector('p');
 
 async function register() {
-    const result = await fetch('https://jsonplaceholder.typicode.com/users', {
+    const result = await fetch('https://jsonplaceholder.typicode.com/posts', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: {
-            name: form.elements.name.value,
+            name: form.elements.nombre.value,
             email: form.elements.email.value,
-            password: form.elements.password.value,
         },
     });
     if (result.status === 200) {

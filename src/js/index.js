67bebb3767ea9c64ec6488464245c73ab5b03749
albacandelario <alba.candelario@gracia.lasalle.cat>
2021-11-30 17:24:59 +0100

    regex and sesionstorage

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,10 @@ const form = document.querySelector('form');
 const button = document.getElementById('btnregister');
 const p = document.querySelector('p');
 
+const regexName = /^[A-Za-z ]+$/;
+const regexMail = /^\w+([\.\+\-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+const regexPassword = /^[A-Za-z0-9]{5,}$/;
+
 async function register() {
     const result = await fetch('https://jsonplaceholder.typicode.com/users', {
         method: 'POST',
@@ -16,11 +20,19 @@ async function register() {
     });
     if (result.status === 201) {
         p.innerHTML = '¡Se ha registrado con éxito!';
+        const resultJson = await result.json();
+        sessionStorage.setItem('id', resultJson.id);
+        console.log(resultJson.id);
     } else {
         p.innerHTML = '¡Oh no, ha ocurrido un error!';
     }
 }
 
+form.addEventListener('keyup', () => {
+    button.disabled = !(regexName.test(form.elements.nombre.value)
+    && regexMail.test(form.elements.email.value)
+    && regexPassword.test(form.elements.password.value));
+});
 button.addEventListener('click', async () => {
     await register();
 });
